1dcb8e3f3a3513398637896336c114c394d76e2c
Clément Pascutto <clement.pascutto@ens.fr>
2015-11-18 16:35:33 +0100

    Added loop guard in headers

--- graph.h
+++ graph.h
@@ -1,5 +1,10 @@
+#ifndef _GRAPH_HEADER_
+#define _GRAPH_HEADER_
+
 typedef struct {
 	int x;
 	int y;
 	double w;
 } edge;
+
+#endif
--- sort.h
+++ sort.h
@@ -1,6 +1,11 @@
+#ifndef _SORT_HEADER_
+#define _SORT_HEADER_
+
 #include "graph.h"
 
 void sort(edge*, int);
 void swap(edge*, int, int);
 void insertion_sort(edge*, int, int);
 void quick_sort(edge*, int, int);
+
+#endif
