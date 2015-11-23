38dc3b1d15c780e840eb08c7516afa255869ae8e
DUSMANU Mihai <mihai.dusmanu@yahoo.com>
2015-11-23 22:30:28 +0100

    Added stl sort for comparison.

---
+++ Performance/test_stl_sort.cpp
@@ -0,0 +1,42 @@
+#include <cstdio>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#define kMaxN 1000000
+
+using namespace std;
+
+struct edge {
+    int x, y;
+    double w;
+};
+
+edge v[kMaxN];
+
+struct edge_less {
+    bool operator() (edge const& a, edge const& b) {
+        return a.w < b.w;
+    }
+};
+
+int main() {
+    int n = kMaxN;
+    clock_t begin, end;
+
+    srand(time(NULL));
+
+    for (int i = 0; i < n; ++i) {
+        v[i].x = rand() % 100;
+        v[i].y = rand() % 100;
+        v[i].w = ((double) rand()) / 666013. * 123456.;
+    }
+
+    begin = clock();
+    sort(v, v + n + 1, edge_less());
+    end = clock();
+
+    printf("Time: %lf\n", (double) (end - begin) / CLOCKS_PER_SEC);
+
+    return 0;
+}
+
