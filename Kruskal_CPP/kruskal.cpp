75252fb702d1bcd18e1e8eb9ad99f2d38bc1cbc7
DUSMANU Mihai <mihai.dusmanu@yahoo.com>
2015-11-24 00:23:36 +0100

    Added a tested CPP version of Kruskal.

---
+++ Kruskal_CPP/kruskal.cpp
@@ -0,0 +1,58 @@
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int parent[200001], rk[200001];
+
+int compress(int node) {
+    return (parent[node] == -1 ? node : parent[node] = compress(parent[node]));
+}
+
+int main() {
+    freopen("test.in", "r", stdin);
+
+    int n, m;
+    scanf("%d%d", &n, &m);
+    
+    vector < pair < double, pair < int, int > > > edges;
+    for (int i = 0; i < m; ++i) {
+        int x, y;
+        double w;
+        scanf("%d%d%lf", &x, &y, &w);
+        edges.push_back(make_pair(w, make_pair(x, y)));
+    }
+
+    sort(edges.begin(), edges.end());
+
+    for (int i = 0; i < n; ++i) {
+        parent[i] = - 1;
+        rk[i] = 1;
+    }
+
+    double ans = 0;
+    vector < pair < int, int > > v;
+    for (int i = 0; i < edges.size(); ++i) {
+        int rx = compress(edges[i].second.first);
+        int ry = compress(edges[i].second.second);
+        if (rx != ry) {
+            if (rk[rx] > rk[ry]) {
+                parent[ry] = rx;
+                rk[rx] += rk[ry];
+            } else {
+                parent[rx] = ry;
+                rk[ry] += rk[rx];
+            }
+            v.push_back(edges[i].second);
+            ans += edges[i].first;
+        }
+    } 
+
+    printf("%lf\n%d\n", ans, (int)v.size());
+
+    for (int i = 0; i < v.size(); ++i)
+        printf("%d %d\n", v[i].first, v[i].second);
+
+    return 0;
+}
